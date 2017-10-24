5f67b47e0806d34a4456aa13cc12297fee998d27
Ravichandran Jv <ravjv2013@gmail.com>
2017-10-24 17:45:55 +0530

    Add files via upload

--- test/missingaddedwordtest.js
+++ test/missingaddedwordtest.js
@@ -3,13 +3,13 @@ var assert=require('assert');
 
 describe('#get missing word', function() {
 	it('should return missing word',function(){
-		var ob=new app.test1("missing word","word");
+		var ob=new app.twoscenarios("missing word","word");
 		assert.equal("missing",ob.missingwordscenariotest());
 	});
 });
 describe('#get added word', function() {
 	it('should return added word',function(){
-		var ob=new app.test1("word","word added");
+		var ob=new app.twoscenarios("word","word added");
 		assert.equal("added",ob.addedwordscenariotest());
 	});
 });
